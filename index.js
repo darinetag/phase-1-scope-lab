4d2c764ace922b17c776b9cc5ab027e95bb0c311
darinetag <tagdarinefatma@gmail.com>
2023-08-11 13:50:44 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase() 
+}
+console.log(upperCaseCustomerName)
+  function setBestCustomer(){
+    bestCustomer ='not bob'
+  }
+  console.log(setBestCustomer)
+  function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+  }
+  console.log(overwriteBestCustomer)
+const leastFavoriteCustomer = 'Naomi'
+function changeLeastFavoriteCustomer(){
+leastFavoriteCustomer = 'sun'
+}
+console.log(changeLeastFavoriteCustomer)
